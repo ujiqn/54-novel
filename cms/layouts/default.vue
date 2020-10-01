2ffaf055be5427e7db2e80485978d7e1de83dbda
君塚史高 <f.kimizuka@gmail.com>
2020-10-01 19:38:30 +0900

    add source code

---
+++ cms/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="wrapper">
+    <div id="fb-root"></div>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121940062-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-121940062-1');
+    </script>
+    <Nuxt />
+  </div>
+</template>

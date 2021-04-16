3f4993ea7f26f4f49e666d8d790e3917b13bb4f0
kimizuka <f.kimizuka@gmail.com>
2021-04-16 12:39:28 +0900

    remove space

--- cms/next/next.config.js
+++ cms/next/next.config.js
@@ -1,6 +1,6 @@
-const isProd = process.env.NODE_ENV == 'production';
+const isProd = process.env.NODE_ENV === 'production';
 const url = isProd ? '/54-novel/' : '';
 
 module.exports = {
-    assetPrefix: url
+  assetPrefix: url
 };

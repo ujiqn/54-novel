086c48e226416260ae613a7c85a617bdd06fe5fa
kimizuka <f.kimizuka@gmail.com>
2021-06-03 23:55:06 +0900

    description変更

--- cms/next/src/pages/_document.tsx
+++ cms/next/src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default class Layout extends Document {
         <Head>
           <title>54字の物語ジェネレーター #54字の文学賞</title>
           <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,shrink-to-fit=no" />
-          <meta name="description" content="『54字の物語』重版記念！54字詰めの原稿用紙で物語をつくりましょう！" />
+          <meta name="description" content="54字ぴったりの物語をつくりましょう。" />
           <meta name="keywords" content="54字の物語,#54字の文学賞,氏田雄介" />
           <meta name="author" content="氏田雄介" />
           <meta name="generator" content="microCMS" />
@@ -24,10 +24,10 @@ export default class Layout extends Document {
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
           <meta property="og:site_name" content="54字の物語ジェネレーター #54字の物語" />
-          <meta property="og:description" content="『54字の物語』重版記念！54字詰めの原稿用紙で物語をつくりましょう！" />
+          <meta property="og:description" content="54字ぴったりの物語をつくりましょう。" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="54字の物語ジェネレーター #54字の物語" />
-          <meta name="twitter:description" content="『54字の物語』重版記念！54字詰めの原稿用紙で物語をつくりましょう！" />
+          <meta name="twitter:description" content="54字ぴったりの物語をつくりましょう。" />
           <meta name="twitter:image:src" content="https://54.ujiqn.com/ogp.png" />
           <meta name="format-detection" content="telephone=no" />
           <link rel="shortcut icon" href="https://54.ujiqn.com/favicon.ico" />
---
+++ docs/_next/static/LdhuQukwH37fuknss2Pr9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F6424d9017b83d4cce713.css","static\u002Fchunks\u002Fpages\u002Findex-e7551f6269b5cf6a1f10.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3cd5ff4acff9d089b5d9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
